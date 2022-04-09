eb671b88848e70c2f80c9124c5f2c13e1b984ead
Justin P <j.planovsky@gmail.com>
2022-04-09 22:52:21 +0000

    Fix remove all throwables when use one

--- client/main.lua
+++ client/main.lua
@@ -160,7 +160,7 @@ CreateThread(function()
         if LocalPlayer.state.isLoggedIn then
             local ped = PlayerPedId()
             if CurrentWeaponData and next(CurrentWeaponData) then
-                if IsPedShooting(ped) or IsControlJustPressed(0, 24) then
+                if IsPedShooting(ped) then
                     local weapon = GetSelectedPedWeapon(ped)
                     if CanShoot then
                         if weapon and weapon ~= 0 and QBCore.Shared.Weapons[weapon] then
@@ -169,22 +169,49 @@ CreateThread(function()
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "snowball", 1)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_pipebomb" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_pipebomb", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_molotov" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_molotov", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_stickybomb" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_stickybomb", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_grenade" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_grenade", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_bzgas" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_bzgas", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_proxmine" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_proxmine", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_ball" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_ball", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_smokegrenade" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_smokegrenade", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             elseif QBCore.Shared.Weapons[weapon]["name"] == "weapon_flare" then
                                 TriggerServerEvent('QBCore:Server:RemoveItem', "weapon_flare", 1)
+                                SetCurrentPedWeapon(ped, `WEAPON_UNARMED`, true)
+                                RemoveAllPedWeapons(ped, true)
+                                TriggerEvent('weapons:client:SetCurrentWeapon', nil, false)
                             else
                                 if ammo > 0 then
                                     MultiplierAmount = MultiplierAmount + 1
